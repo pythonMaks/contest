ae90bc269d7379af7c5930275b5decbdf7e4089a
pythonMaks <python.maxim@yandex.ru>
2023-07-12 12:23:58 +0300

    second

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Task, Test
+from .models import Task, Test, Submission
 
-admin.register(Test)
+admin.site.register(Test)
+admin.site.register(Submission)
+
+  
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
