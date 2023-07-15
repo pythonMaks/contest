720f89e9a35b5bf11ef4897802c764397a5f7729
pythonMaks <python.maxim@yandex.ru>
2023-07-15 09:22:53 +0300

    confirm delete/edit

--- groups/urls.py
+++ groups/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('group/', views.create_group, name='create_group'),
     path('groups/', views.view_groups, name='groups'),
     path('group/<int:group_id>/', views.view_group, name='group_detail'),
-    path('group_delete/<int:group_id>', views.confirm_delete, name='delete_group'),
+    path('group_delete/<int:group_id>', views.delete_group, name='delete_group'),
     path('edit_groups/<int:group_id>', views.edit_group, name='edit_group'),
    
 ]
