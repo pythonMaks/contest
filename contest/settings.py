3bf6a5d0b5355d5a9ea5cc1d09cbda2e71d523a3
pythonMaks <python.maxim@yandex.ru>
2023-07-15 15:33:35 +0300

    404

--- contest/settings.py
+++ contest/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Password validation
@@ -142,11 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
