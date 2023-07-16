65c059873455bc563eea6d675d96b63f51e9b4a7
pythonMaks <python.maxim@yandex.ru>
2023-07-16 08:47:52 +0300

    fix table links

--- groups/templatetags/my_extras.py
+++ groups/templatetags/my_extras.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def average(value_list):
     return round((sum(value_list) / len(value_list)), 2) if value_list else 0
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
