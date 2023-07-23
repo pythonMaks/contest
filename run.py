fe23efc4617baf57ec6418c1abb1c81332e27cd1
pythonMaks <python.maxim@yandex.ru>
2023-07-23 10:00:19 +0300

    tg_bot

--- run.py
+++ run.py
@@ -1,15 +1,12 @@
-from multiprocessing import Process
-from django.core.management import call_command
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django.setup()
 
 def run_bot():
     from users.bots.tg_bot import main
     main()
 
-def run_django():
-    call_command('runserver', '0.0.0.0:8000')
-
 if __name__ == '__main__':
-    p1 = Process(target=run_bot)
-    p1.start()
-    p2 = Process(target=run_django)
-    p2.start()
+    run_bot()
