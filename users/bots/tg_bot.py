23e2f77adbdab2c06a629cbdb0f85c76032de0b8
pythonMaks <python.maxim@yandex.ru>
2023-07-23 11:52:15 +0300

    tg_bot

--- users/bots/tg_bot.py
+++ users/bots/tg_bot.py
@@ -25,7 +25,7 @@ async def handle_code(message: types.Message):
     try:
         user = await sync_to_async(User.objects.get, thread_sensitive=True)(access_code=user_message)
         user.chat_id = chat_id
-        user.save()
+        await sync_to_async(user.save, thread_sensitive=True)()
         await message.reply(f'Добро пожаловать, {user.username}! Вы успешно подписались на оповещения!')
     except User.DoesNotExist:
         await message.reply('Введен не корректный код! Проверьте код в вашем профиле http://13.50.99.201:8000/profile/')
